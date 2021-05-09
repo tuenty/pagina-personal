5c55744479b28bd8335b16972ddc3dd60bb0825d
Gabriel Nuñez <gabs88.nu@gmail.com>
2021-05-09 19:33:20 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const { render } = require("ejs");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("index.html");
+});
+
+module.exports = router;
